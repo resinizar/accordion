3f531d31eb43608575eee28166a6de166a37a8d6
Gabriel Cano <grc@bonnibel.lan>
2022-09-12 21:18:04 -0600

    Put accordion buttons into a map & give search results from central C button
    Right now all buttons are an array of three ints, even the buttons that only
    sound one pitch, not a chord. I think the benefits of 1) having the buttons fit
    neatly into a map, which 2) makes the search function simpler, are worth it.

--- main.go
+++ main.go
@@ -25,100 +25,78 @@ func min7(root int) int {
 }
 
 type accordion struct {
-	roots [20]int
-	maj3s [20]int
-	majs  [20][3]int
-	mins  [20][3]int
-	dom7s [20][3]int
-	dim7s [20][3]int
+	numRows    int
+	numButtons int
+	centerInd  int
+	rows       map[string][20][3]int
 }
 
 func newAccordion() *accordion {
 	a := accordion{}
 
-	for i := range a.roots {
+	// always a full size accordion
+	// control indexing for other sizes
+	a.rows = make(map[string][20][3]int)
+
+	var new_bs [20][3]int
+	for i := 0; i < 20; i++ {
 		if i == 0 {
-			a.roots[i] = 9 // bottom note
+			new_bs[i] = [3]int{9, 9, 9} // bottom note of full size
 		} else {
-			a.roots[i] = fifth(a.roots[i-1])
+			prev_note := new_bs[i-1][0]
+			new_bs[i] = [3]int{fifth(prev_note), fifth(prev_note), fifth(prev_note)}
 		}
 	}
+	a.rows["roots"] = new_bs
 
-	for i := range a.maj3s {
-		r := a.roots[i]
-		a.maj3s[i] = maj3(r)
+	for i := 0; i < 20; i++ {
+		r := a.rows["roots"][i][0]
+		new_bs[i] = [3]int{maj3(r), maj3(r), maj3(r)}
 	}
+	a.rows["maj3s"] = new_bs
 
-	for i := range a.majs {
-		r := a.roots[i]
-		a.majs[i] = [3]int{r, a.maj3s[i], fifth(r)}
+	for i := 0; i < 20; i++ {
+		r := a.rows["roots"][i][0]
+		new_bs[i] = [3]int{r, maj3(r), fifth(r)}
 	}
+	a.rows["majs"] = new_bs
 
-	for i := range a.mins {
-		r := a.roots[i]
-		a.mins[i] = [3]int{r, min3(r), fifth(r)}
+	for i := 0; i < 20; i++ {
+		r := a.rows["roots"][i][0]
+		new_bs[i] = [3]int{r, min3(r), fifth(r)}
 	}
+	a.rows["mins"] = new_bs
 
-	for i := range a.dom7s {
-		r := a.roots[i]
-		a.dom7s[i] = [3]int{r, maj3(r), maj7(r)}
+	for i := 0; i < 20; i++ {
+		r := a.rows["roots"][i][0]
+		new_bs[i] = [3]int{r, maj3(r), maj7(r)}
 	}
+	a.rows["dom7s"] = new_bs
 
-	for i := range a.dim7s {
-		r := a.roots[i]
-		a.dim7s[i] = [3]int{min7(r), r, min3(r)}
+	for i := 0; i < 20; i++ {
+		r := a.rows["roots"][i][0]
+		new_bs[i] = [3]int{min7(r), r, min3(r)}
 	}
+	a.rows["dim7s"] = new_bs
 
+	// for now only handling full size
+	// later pass these in
+	a.numButtons = 20
+	a.numRows = 6
+	a.centerInd = 9
 	return &a
 }
 
-func (a *accordion) searchNote(note int) {
-	for i, n := range a.roots {
-		if n == note {
-			fromC := i - 9
-			fmt.Printf("roots button %d\n", fromC)
-		}
-	}
+func (a *accordion) searchNote(searchNote int) {
 
-	for i, n := range a.maj3s {
-		if n == note {
-			fromC := i - 9
-			fmt.Printf("maj3s button %d\n", fromC)
-		}
-	}
-
-	for i, ch := range a.majs {
-		for _, n := range ch {
-			if n == note {
-				fromC := i - 9
-				fmt.Printf("majs button %d\n", fromC)
-			}
-		}
-	}
-
-	for i, ch := range a.mins {
-		for _, n := range ch {
-			if n == note {
-				fromC := i - 9
-				fmt.Printf("mins button %d\n", fromC)
-			}
-		}
-	}
-
-	for i, ch := range a.dom7s {
-		for _, n := range ch {
-			if n == note {
-				fromC := i - 9
-				fmt.Printf("dom7s button %d\n", fromC)
-			}
-		}
-	}
-
-	for i, ch := range a.dim7s {
-		for _, n := range ch {
-			if n == note {
-				fromC := i - 9
-				fmt.Printf("dim7s button %d\n", fromC)
+	for rowName, row := range a.rows {
+		for i, ch := range row {
+			for _, n := range ch {
+				if n == searchNote {
+					fromC := i - a.centerInd
+					fmt.Printf("%s button %d\n", rowName, fromC)
+					break
+				}
 			}
 		}
 	}
